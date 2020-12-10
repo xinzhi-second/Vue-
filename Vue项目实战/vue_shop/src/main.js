b8df5d035b53dda342117fb2cbcf0fb00bef98ee
hli <lihu9487>
2020-12-10 22:28:12 +0800

    用户列表功能开发

--- Vue项目实战/vue_shop/src/main.js
+++ Vue项目实战/vue_shop/src/main.js
@@ -13,6 +13,15 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios 请求拦截
+axios.interceptors.request.use(config => {
+    const url = config.url;
+    if(url !== 'login') {
+        config.headers.Authorization = window.sessionStorage.getItem('token');
+    }
+    return config; 
+})
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
